9ad9060342ef7d5ddddc8e281f3cc7e016fc2a53
toosloe <chloemarileejones@gmail.com>
2025-03-05 23:48:30 -0500

    Finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = [];
+    for(let i = 0;i < string.length;i++){
+            arr.unshift(string.at(i));
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
