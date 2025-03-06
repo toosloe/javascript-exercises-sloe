6dcb48738456a41571709da74e82a0e9146f8318
toosloe <chloemarileejones@gmail.com>
2025-03-05 20:14:31 -0500

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, x) {
+    let tempString = string;
+    if(x < 0){
+        return "ERROR";
+    }
+    else if(x == 0){
+        return "";
+    }
+    for(let i = 1; i < x; i++){
+        string = string.concat(tempString)
+    }
+    return string;
 };
 
 // Do not edit below this line
