377c36a7d6480ed469a3d5769d09acfe4972886b
toosloe <chloemarileejones@gmail.com>
2025-03-06 22:35:17 -0500

    Finished 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x,y,tot) {
+    tot = 0;
+    if(x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)){
+        return 'ERROR';
+    }
 
+    if(x>y){
+        for(let i=y;i<=x;i++){
+            tot += i;
+        }
+        return tot;
+    }
+    else if(y>x){
+        for(let i=x;i<=y;i++){
+            tot += i;
+        }
+        return tot;
+    }
+    else return tot;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(leap) {
+    if(leap % 4 == 0){
+        if(leap%100==0 && leap%400==0){
+            return true;
+        }
+        else if(leap%100==0){
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
