1212db58db61419aa0bdb41bf0a829fda9c093ca
toosloe <chloemarileejones@gmail.com>
2025-03-06 22:49:52 -0500

    Finished.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Far) {
+  return Math.round(((Far-32)/1.8)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Cel) {
+  return Math.round(((Cel * 1.8)+32)*10)/10;
 };
 
 // Do not edit below this line
